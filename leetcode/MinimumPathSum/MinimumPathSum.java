a69abbdc54f249e14b98bf9aa3d0b00611029b5c
Spicy Zinc <angxin880@gmail.com>
2017-08-28 00:06:34 -0700

    a typical 2d DP, find the minimum path sum, top left, first row, first column, rest, return dp[m-1][n-1]

--- leetcode/MinimumPathSum/MinimumPathSum.java
+++ leetcode/MinimumPathSum/MinimumPathSum.java
@@ -3,7 +3,6 @@ Given a m x n grid filled with non-negative numbers,
 find a path from top left to bottom right which minimizes the sum of all numbers along its path.
 
 idea:
-
 a typical DP
 set up first row and first column value
 then for coming to a cell, it could either from left or from top
@@ -26,32 +25,31 @@ public class MinimumPathSum {
 			System.out.print("\n");
         }
 		
-		MinimumPathSum aTest = new MinimumPathSum();
-		System.out.println("For this grid the minimum path sum is " + aTest.minPathSum(grid));
+		MinimumPathSum eg = new MinimumPathSum();
+		System.out.println("For this grid the minimum path sum is " + eg.minPathSum(grid));
 	}
-    public int minPathSum(int[][] grid) {         
-        if (grid == null || grid.length == 0) {
-            return 0;             
+    // typical DP
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        // first value
+        dp[0][0] = grid[0][0];
+        // first row
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
         }
-        int m = grid.length; // rows
-		int n = grid[0].length; // columns
-        int[][] min = new int[m][n];
- 
-        min[0][0] = grid[0][0]; // start from top left
-        for (int j = 1; j < n; j++) {
-            min[0][j] = min[0][j-1] + grid[0][j];  
-        }
- 
+        // first column
         for (int i = 1; i < m; i++) {
-            min[i][0] = min[i-1][0] + grid[i][0];
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
         }
- 
+        // the rest
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                min[i][j] = Math.min( min[i-1][j], min[i][j-1] ) + grid[i][j];				
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
             }
         }
- 
-        return min[m-1][n-1];
+        
+        return dp[m - 1][n - 1];
     }
 }
