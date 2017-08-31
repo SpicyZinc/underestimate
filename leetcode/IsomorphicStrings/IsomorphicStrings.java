8b72f7e350c50abaf46a9e50c174999ead7e362a
Liang Xin <angxin880@gmail.com>
2017-08-31 11:44:30 -0700

    isomorphic string rewrite, s to t map, conciser

--- leetcode/IsomorphicStrings/IsomorphicStrings.java
+++ leetcode/IsomorphicStrings/IsomorphicStrings.java
@@ -8,56 +8,25 @@ Given "foo", "bar", return false.
 Given "paper", "title", return true.
 
 idea:
-
-
+same as word pattern
+note: s to t map or t to s map
 */
-
-import java.util.*;
 
 public class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
-        if ( s == null || t == null ) {
-            return false;
-        }
+        if (s.length() != t.length()) return false;
 
-        if ( s.length() != t.length() ) {
-            return false;
-        }
-
-        if ( s.length() == 0 && t.length() == 0 ) {
-            return true;
-        }
-
-        HashMap<Character, Character> hm = new HashMap<Character, Character>();
+        Map<Character, Character> hm = new HashMap<Character, Character>();
         for (int i = 0; i < s.length(); i++) {
-            Character c1 = s.charAt(i);
-            Character c2 = t.charAt(i);
-
-            Character c = getKey(hm, c2);
-            if ( c != null && c != c1 ) {
-                return false;
-            }
-            else if ( hm.containsKey(c1) ) {
-                if ( hm.get(c1) != c2 ) {
-                    return false;
-                }
-            }
-            else {
-                hm.put(c1, c2);
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (hm.containsKey(sChar)) {
+                if (hm.get(sChar) != tChar) return false;
+            } else {
+                if (hm.containsValue(tChar)) return false;
+                hm.put(sChar, tChar);
             }
         }
-
         return true;
-    }
-
-
-    private Character getKey(HashMap<Character, Character> map, Character target) {
-        for ( Map.Entry<Character, Character> entry : map.entrySet() ) {
-            if ( entry.getValue().equals(target) ) {
-                return entry.getKey();
-            }
-        }
-
-        return null;
     }
 }
