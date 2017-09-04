142d32c44c87ebc7fe62b7da1f9d1925225a9867
Spicy Zinc <angxin880@gmail.com>
2017-09-04 00:58:04 -0700

    Sliding window problem, substring must be consecutive, meeting a standing out stick, left should be moved, left++ points to a new window left

--- leetcode/PermutationInString/PermutationInString.java
+++ leetcode/PermutationInString/PermutationInString.java
@@ -16,33 +16,35 @@ The input strings only contain lower case letters.
 The length of both given strings is in range [1, 10,000].
 
 idea:
+https://discuss.leetcode.com/topic/87884/8-lines-slide-window-solution-in-java/2
+note: this is to get substring NOT subsequence, must be consecutive
 1. digging hole, and fill it out
+    挖的坑填上
+    没挖的填上超出要减去
 2. basic idea, 2 steps
 	How do we know string p is a permutation of string s?
 	Easy, each character in p is in s too. So we can abstract all permutation strings of s to a map (Character -> Count). i.e. abba -> {a:2, b:2}.
 	Since there are only 26 lower case letters in this problem, we can just use an array to represent the map.
 	
-	How do we know string s2 contains a permutation of s1? We just need to create a sliding window with length of s1, move from beginning to the end of s2.
+	How do we know string s2 contains a permutation of s1? We just need to create a sliding window with length of s1,
+    move from beginning to the end of s2.
 	When a character moves in from right of the window, we subtract 1 to that character count from the map.
 	When a character moves out from left of the window, we add 1 to that character count.
 	So once we see all zeros in the map, meaning equal numbers of every characters between s1 and the substring in the sliding window, we know the answer is true.
-	挖的坑填上
-	没挖的继续减
 */
 
 public class PermutationInString {
 	// method 1
     public boolean checkInclusion(String s1, String s2) {
-        int[] count = new int[26];
+        int[] map = new int[128];
         for (int i = 0; i < s1.length(); i++) {
-            char c = s1.charAt(i);
-            count[c - 'a']--;
+            map[s1.charAt(i)]--;
         }
-        for (int l = 0, r = 0; r < s2.length(); r++) {
-            if (++count[s2.charAt(r) - 'a'] >= 1) {
-                while (--count[s2.charAt(l++) - 'a'] < 0) { }
-            }
-            else if (r - l + 1 == s1.length()) {
+        int left = 0;
+        for (int right = 0; right < s2.length(); right++) {
+            if (++map[s2.charAt(right)] >= 1) {
+                while (--map[s2.charAt(left++)] < 0) {}
+            } else if (right - left + 1 == s1.length()) {
                 return true;
             }
         }
