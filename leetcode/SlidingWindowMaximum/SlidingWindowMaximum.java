9b6d2f798c3681b088c10c2f603a3123ae9e8ca6
Spicy Zinc <angxin880@gmail.com>
2017-09-05 02:25:46 -0700

    maxHeap or double ended queue can be used, maintain this queue of size <= k is to maintain the window of size k, if looped too many elements then remove left most, and make sure always descending order, in addition, queue saves index

--- leetcode/SlidingWindowMaximum/SlidingWindowMaximum.java
+++ leetcode/SlidingWindowMaximum/SlidingWindowMaximum.java
@@ -26,43 +26,38 @@ Hint:
 How about using a data structure such as deque (double-ended queue)?
 
 idea:
-http://www.shuatiblog.com/blog/2014/07/27/Sliding-Window-Maximum/
-http://n00tc0d3r.blogspot.com/2013/04/sliding-window-maximum.html
-https://hellosmallworld123.wordpress.com/2014/05/28/sliding-window-maximum/
+https://segmentfault.com/a/1190000003903509
+double ended queue
 
-queue position 0 keeps the max's index so far
-if not within the window, should remove it
+Each element in the queue is being inserted and then removed at most once. 
+Therefore, the total number of insert and delete operations is 2n. It is an O(n) solution.
 
-Each element in the list is being inserted and then removed at most once. 
-Therefore, the total number of insert and delete operations is 2n.
-Therefore it is an O(n) solution.
+sliding window length = (length - 1) - (k - 1) + 1 = length - k + 1
+
+maintain a queue less than or equal to k, this guarantees window of size k
 */
 public class SlidingWindowMaximum {
     // best solution
-    public int[] slidingWindowMax(int[] array, int w) {
-        if ( array.length == 0 || array == null ) {
-            return array;
-        }
-        int[] ans = new int[array.length - w + 1];
-        List<Integer> q = new LinkedList<Integer>();
-        // Queue stores indices of array, and values are in decreasing order.
-        // In this way, the top element in queue is the max in window
-        for (int i = 0; i < array.length; i++) {
-            // 1. remove element from head until first number within window
-            if (!q.isEmpty() && q.get(0) + w <= i) {
-                // it's OK to change 'while' to 'if' in the line above because we actually remove 1 element at most
-                q.remove(0);
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || nums == null) return nums;
+        int[] ans = new int[nums.length - k + 1];
+        List<Integer> dequeue = new LinkedList<Integer>();
+        // dequeue stores indices of nums, and corresponding values are in decreasing order. This way, the head in queue is always the max
+        for (int i = 0; i < nums.length; i++) {
+            // 1. remove left most element from queue so queue size is k
+            // i - queue.get(0) + 1 = k so queue.get(0) + k > i
+            if (!dequeue.isEmpty() && dequeue.get(0) + k <= i) dequeue.remove(0);
+            // 2. before inserting i into queue, remove indices pointing to value smaller than nums[i] from queue tail to make sure descending order            
+            while (!dequeue.isEmpty() && nums[dequeue.get(dequeue.size() - 1)] < nums[i]) {
+                dequeue.remove(dequeue.size() - 1);
             }
-            // 2. before inserting i into queue, remove from the tail of the queue indices with smaller value array[i]
-            while (!q.isEmpty() && array[q.get(q.size() - 1)] <= array[i]) {
-                q.remove(q.size() - 1);
-            }
-            q.add(i);
-            // 3. set the max value in the window (always the top number in queue)
-            if (i + 1 >= w) {
-                ans[i + 1 - w] = array[q.get(0)];
+            dequeue.add(i);
+            // 3. i + 1 is the length, start to collect max
+            if (i + 1 >= k) {
+                ans[i + 1 - k] = nums[dequeue.get(0)];
             }
         }
+        
         return ans;
     }
 
@@ -93,27 +88,3 @@ public class SlidingWindowMaximum {
         return max;
     }
 }
-
-public static void maxSlidingWindow(int a[], int n, int w, int b[]) { 
-    if (n <= 0 || n < w) {
-        return;  
-    }
-    LinkedList<Integer> l = new LinkedList<Integer>();  
-    for (int i = 0; i < w; i++) {  
-        while (l.size() > 0 && a[l.peekLast()] <= a[i]) {
-            l.pollLast();  
-        }
-        l.add(i);  
-    }
-    b[0] = a[l.peekFirst()];  
-    for (int i = w; i < n; i++) {  
-        while (l.size() > 0 && l.peekFirst() <= i-w) {
-            l.removeFirst();  
-        }
-        while (l.size()>0 && a[l.peekLast()] <= a[i]) {
-            l.pollLast();
-        }
-        l.add(i);  
-        b[i-w+1] = a[l.peekFirst()];  
-    }
-}  
