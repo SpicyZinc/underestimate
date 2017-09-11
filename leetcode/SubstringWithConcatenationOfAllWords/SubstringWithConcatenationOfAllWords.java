3c3252390c216937c0a9c1eaea614f01a057a885
Spicy Zinc <angxin880@gmail.com>
2017-09-10 22:00:07 -0700

    substring indices in string with all concatenated words, each char in s as a start, try all them by for loop

--- leetcode/SubstringWithConcatenationOfAllWords/SubstringWithConcatenationOfAllWords.java
+++ leetcode/SubstringWithConcatenationOfAllWords/SubstringWithConcatenationOfAllWords.java
@@ -1,187 +1,72 @@
 /*
-Substring with Concatenation of All Words
-
-a string, S, 
-a list of words, L, that are all of the same length. 
-Find all starting indices of substring(s) in S 
-that is a concatenation of each word in L exactly once and without any intervening characters.
-
-Attention: concatenation of each word
-		   each word in the concatenation shows exactly once
-		   exactly once
+You are given a string, s, and a list of words, words, that are all of the same length.
+Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
 
 For example, given:
-S: "barfoothefoobarman"
-L: ["foo", "bar"]
+s: "barfoothefoobarman"
+words: ["foo", "bar"]
 
 You should return the indices: [0,9].
+(order does not matter).
 
 idea:
-one substring of length m * n moves from left to right, 
-starting from each i position, step length is 1, get a substring containing m * n length,
-judge if each word in words[] only appears one time 
+note: concatenation of each word, each word in the concatenation shows exactly once 
 
-Attention: words[] have duplicate words
-hashmap must record their appearing times exactly
-duplicate words are counted as different words, they must appear the same times as in words[]
+take each char in s as start of a possible word
+if found this possible word is one of the words[] array, continue in n (length of words[])
+if not, break early, go to next char in s by i++
 
-note: can repetitively count word appearing in last substring to be a word in a new substring
-break is important
+note: make a copy of hm in each iteration
 */
+
 import java.util.*;
 
 public class SubstringWithConcatenationOfAllWords {
-	private String S;
-	private String[] L;
-
-	// constructor
-	public SubstringWithConcatenationOfAllWords(String string, String[] list) {
-		S = string;
-		L = list;
-		System.out.println(findSubstring(S, L).toString());
-	}
 	public static void main(String[] args) {
-		// String S = "barfoothefoobarmanbarfoo";
-		// String[] L = {"foo", "bar"};
-		String S = "aaaa";
-		String[] L = {"a", "a"};
-		new SubstringWithConcatenationOfAllWords(S, L);
+		String s = "barfoothefoobarman";
+		String[] words = {"foo", "bar"};
+		// String s = "aaaa";
+		// String[] words = {"a", "a"};
+        SubstringWithConcatenationOfAllWords eg = new SubstringWithConcatenationOfAllWords();
+        System.out.println(eg.findSubstring(s, words));
 	}
 	
-    public ArrayList<Integer> findSubstring(String S, String[] L) {
-        if (L == null || L.length == 0) {
-			return null;
-        }
-		int l = S.length();	
-        int n = L.length;
-		int m = L[0].length();		
-        ArrayList<Integer> res = new ArrayList<Integer>();        
-        Map<String, Integer> covered = new HashMap<String, Integer>();
-        for (int j=0; j<n; j++) {
-            if (covered.containsKey(L[j])) {
-                covered.put(L[j], covered.get(L[j]) + 1);
-            }
-			else {
-                covered.put(L[j], 1);
-            }
-        }
-        // pointer walks through the String S
-        int i = 0;
-        while (l-i >= m * n) {
-			// Constructs a new HashMap with the same mappings as the specified Map
-            // HashMap repetitively used to decide when to add the index to retured list
-            // so need to construct every time 
-            Map<String, Integer> temp = new HashMap<String, Integer>(covered);
-			// pick up n substrings of length m
-            for (int j=0; j<n; j++) {
-				// first pick up a substring of length m
-                String tmpString = S.substring(i + m * j, i + m * (j + 1));
-                if (temp.containsKey(tmpString)) {
-                    if (temp.get(tmpString) == 1) {
-                        temp.remove(tmpString);
-                    }
-                    else {
-                        temp.put(tmpString, temp.get(tmpString) - 1);
-                    }
-                }
-				else
-                    break;
-            }
-			if (temp.size() == 0) {
-				res.add(i);
-			}
-			i++;
-			/*
-			// it is just another way to understand this question
-			// a word appearing in last substring cannot be counted a word in a new substring
-			// no need to think so be careful
-            if (temp.size() == 0) {
-				res.add(i);
-				i += m*n;
-			}
-			else {
-				i++;
-			} 
-			*/           
-        }
-        return res; 		
-    }
-
-    // self written, passed test
-    public List<Integer> findSubstring(String S, String[] L) {
-        int l = S.length();
-        int n = L.length;
-        int m = L[0].length();
-        
-        Map<String, Integer> discovered = new HashMap<String, Integer>();
-        for (int i = 0; i < n; i++) {
-            if (discovered.containsKey(L[i])) {
-                discovered.put(L[i], discovered.get(L[i]) + 1);
-            }
-            else {
-                discovered.put(L[i], 1);
-            }
-        }
-        
-        int i = 0;
-        List<Integer> ret = new ArrayList<Integer>();
-        while (l - i >= m * n) {
-            Map<String, Integer> covered = new HashMap<String, Integer>(discovered);
-            for (int j = 0; j < n; j++) {
-                String tempString = S.substring( i + m * j, i + m * (j + 1) );
-                if (covered.containsKey(tempString)) {
-                    if (covered.get(tempString) == 1) {
-                        covered.remove(tempString);
-                    }
-                    else {
-                        covered.put(tempString, covered.get(tempString) - 1);
-                    }
-                }
-                else {
-                    break;
-                }
-            }
-            if (covered.size() == 0) {
-                ret.add(i);
-            }
-            i++;
-        }
-        return ret;
-    }
-    // recent self written again
     public List<Integer> findSubstring(String s, String[] words) {
-        int size = s.length();
-        int n = words.length;
-        int m = words[0].length();
-        
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        List<Integer> result = new ArrayList<Integer>();
+        if (s.length() == 0 || s == null || words.length == 0) {
+            return result;
+        }
+        Map<String, Integer> hm = new HashMap<String, Integer>();
         for (String word : words) {
             hm.put(word, hm.getOrDefault(word, 0) + 1);
         }
         
-        List<Integer> result = new ArrayList<Integer>();
+        int size = s.length();
+        int n = words.length;
+        int wordLen = words[0].length();
         int i = 0;
-        while (size - i >= m * n) {
-            HashMap<String, Integer> copy = new HashMap<String, Integer>(hm);
+        
+        while (size - i >= wordLen * n) {
+            Map<String, Integer> copyMap = new HashMap<String, Integer>(hm);
+            // get the same count of possible words as the number of words
             for (int j = 0; j < n; j++) {
-                String possibleWord = s.substring(i + m * j, i + m * (j + 1));
-                if (copy.containsKey(possibleWord)) {
-                    if (copy.get(possibleWord) == 1) {
-                        copy.remove(possibleWord);
-                    }
-                    else {
-                        copy.put(possibleWord, copy.get(possibleWord) - 1);
-                    }
-                }
-                else {
-                    break; // note, this saves a lot of time
+                String possibleWord = s.substring(i + j * wordLen, i + (j + 1) * wordLen);
+                // if possibleWord not even exists in map, break early, directly i++
+                // this saves a lot of time
+                if (!copyMap.containsKey(possibleWord)) break;
+                int frequency = copyMap.get(possibleWord);
+                if (frequency > 1) {
+                    copyMap.put(possibleWord, frequency - 1);
+                } else {
+                    copyMap.remove(possibleWord);
                 }
             }
-            if (copy.size() == 0) {
+            if (copyMap.size() == 0) {
                 result.add(i);
             }
             i++;
         }
+        
         return result;
     }
 }
