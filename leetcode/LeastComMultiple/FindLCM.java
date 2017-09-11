bf3f419714c9c503cf40e7408090052aa06f919a
Spicy Zinc <angxin880@gmail.com>
2017-09-10 23:10:18 -0700

    set up minimum factorization and rewrite Least Common Multiple, loop from 1 to <= smaller number, first multiple of bigger one which is also divisible by smaller one will be least common multiple

--- leetcode/LeastComMultiple/FindLCM.java
+++ leetcode/LeastComMultiple/FindLCM.java
@@ -1,36 +1,32 @@
-/**
+/*
 find the least common multiple
 
 idea:
-x divides y "equals to" y is divisible by x
+x divides y == y is divisible by x
 
+find bigger one of the two number
+a * b will be common multiple for sure
+but starting from 1 to <= smaller one of the two number
+first multiple of bigger one also divisible by smaller one will be the result
 */
 
 public class FindLCM {
-        public static int determineLCM(int a, int b) {
-                int num1, num2;
-                if (a > b) {
-                        num1 = a;
-                        num2 = b;
-                } 
-				else {
-                        num1 = b;
-                        num2 = a;
-                }
-                for (int i = 1; i <= num2; i++) {
-					// the first multiple of the larger number of the two 
-					// which can also be divisible by the smaller number	
-					if ((num1 * i) % num2 == 0) { 
-							// key point where lcm is
-							return i * num1; 
-					}
-                }
-                throw new Error("Error");
+    public static int determineLCM(int a, int b) {
+        int bigger, smaller;
+        if (a > b) {
+            bigger = a;
+            smaller = b;
+        } else {
+            bigger = b;
+            smaller = a;
         }
-
-        public static void main(String[] args) {
-                FindLCM lcmTest = new FindLCM();
-                System.out.println("6 and 7 lcm is " + lcmTest.determineLCM(6, 7));
-				System.out.println("15 and 12 lcm is " + lcmTest.determineLCM(15, 12));
+        for (int i = 1; i <= smaller; i++) {
+			// the first multiple of the larger number of the two 
+			// which can also be divisible by the smaller number	
+			if ((bigger * i) % smaller == 0) { 
+				return bigger * i;
+			}
         }
+        return a * b;
+    }
 }
--- leetcode/MinimumFactorization/MinimumFactorization.java
+++ leetcode/MinimumFactorization/MinimumFactorization.java
@@ -1,10 +1,9 @@
-
+/*
 Given a positive integer a, find the smallest positive integer b whose multiplication of each digit equals to a.
 If there is no answer or the answer is not fit in 32-bit signed integer, then return 0.
 
 Example 1
 Input:
-
 48 
 Output:
 68
@@ -15,6 +14,9 @@ Input:
 Output:
 35
 
+idea:
+no idea
+*/
 
 public class MinimumFactorization {
     public int smallestFactorization(int a) {
