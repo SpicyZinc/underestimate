9410726671d32dd03e244c98d8f0af6795f5a9f6
Spicy Zinc <angxin880@gmail.com>
2017-09-18 01:07:41 -0700

    valid palindrome i and ii, start from both head and tail, if not equal return false, for ii skip the char to remove by i + 1 or j - 1

--- leetcode/ValidPalindrome/ValidPalindrome.java
+++ leetcode/ValidPalindrome/ValidPalindrome.java
@@ -10,42 +10,34 @@ Have you consider that the string might be empty? This is a good question to ask
 For the purpose of this problem, we define empty string as valid palindrome. 
 
 idea:
-regex to replaceAll first
-toLowerCase()
+first use regex to replace all non-word character with empty space
+then convert all to toLowerCase()
 \w 	A word character: [a-zA-Z_0-9]
 \W 	A non-word character: [^\w]
 
 even: is ideal, half half compare
-odd: leave the middle one alone, no need to compare, 
-i < s.length() / 2
-key point
-
+odd: leave the middle one alone, no need to compare
+note, i < s.length() / 2, then start from head and tail of the string
 */
 
 public class ValidPalindrome {
 	public static void main(String[] args) {
-		new ValidPalindrome();
-	}
-	// constructor
-	public ValidPalindrome() {
-		System.out.println("a ba is Palindrome: " + isPalindrome("a ba"));
+		ValidPalindrome eg = new ValidPalindrome();
+		System.out.println("a ba is Palindrome: " + eg.isPalindrome("a ba"));
 	}
     public boolean isPalindrome(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (s.length() == 0 || s == null)
-    		return true;		
+        if (s.length() == 0 || s == null) return true;		
 		
 		s = s.replaceAll("\\W", "");
     	s = s.toLowerCase();
-        int i = 0;
+
         int n = s.length();
-		// remember
-		while (i < n/2) {
-			if (s.charAt(i) == s.charAt(n-1-i))
-				i++;
-			else 
-				return false;
+        int i = 0;
+		while (i < n / 2) {
+			if (s.charAt(i) != s.charAt(n-1-i)) {
+			    return false;
+            }
+			i++;
 		}
 		return true;
     }
---
+++ leetcode/ValidPalindromeII/ValidPalindrome.java
@@ -0,0 +1,76 @@
+/*
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+
+idea:
+start from both head and tail of the string
+if char at i and j not equal, break while
+try to skip the character by i + 1 or j - 1
+as long as return true, it is true
+*/
+
+class ValidPalindromeII {
+	public boolean validPalindrome(String s) {
+		int i = 0;
+		int j = s.length() - 1;
+
+		while (i < j && s.charAt(i) == s.charAt(j)) {
+            i++;
+            j--;
+		}
+        if (i >= j || isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1)) {
+            return true;
+        }
+		return false;
+	}
+
+	public boolean isPalindrome(String s, int i, int j) {
+		while (i < j) {
+			if (s.charAt(i) != s.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
+	// TLE
+	public boolean validPalindrome(String s) {
+		int len = s.length();
+		if (len <= 2) {
+			return true;
+		}
+		for (int i = 0; i < len; i++) {
+			String substring = s.substring(0, i) + s.substring(i + 1);
+			if (isPalindrome(substring)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isPalindrome(String s) {
+        if (s.length() == 0 || s == null) return true;
+
+        int n = s.length();
+        int i = 0;
+		while (i < n / 2) {
+			if (s.charAt(i) != s.charAt(n-1-i)) {
+			    return false;
+            }
+			i++;
+		}
+		return true;
+    }
+}
