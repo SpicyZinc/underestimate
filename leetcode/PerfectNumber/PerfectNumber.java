b1f84f1a0a44fe7df14884c3f7f932be211cab39
Spicy Zinc <angxin880@gmail.com>
2017-09-22 10:06:16 -0700

    note corner case 0 and 1, and itself should not be considered

---
+++ leetcode/PerfectNumber/PerfectNumber.java
@@ -0,0 +1,40 @@
+/*
+We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+
+Example:
+Input: 28
+Output: True
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+Note: The input number n will not exceed 100,000,000. (1e8)
+
+idea: just for fun
+*/
+class PerfectNumber {
+	// TLE
+	public boolean checkPerfectNumber(int num) {
+		int sum = 0;
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				sum += i;
+			}
+		}
+		return num == sum;
+	}
+	// till Math.sqrt(num)
+	// note corner case 0 and 1
+	public boolean checkPerfectNumber(int num) {
+        if (num == 0 || num == 1) return false; 
+		int sum = 0;
+		for (int i = 1; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+                if ((num / i) < num) {
+                    sum += i + (num / i);    
+                } else {
+                    sum += i;
+                }
+			}
+		}
+		return num == sum;	
+	}
+}
