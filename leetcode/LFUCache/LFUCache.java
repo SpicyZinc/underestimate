732418c7d69bbd21ab2b6ca3f70d7a380e96631a
Spicy Zinc <angxin880@gmail.com>
2017-09-27 23:17:45 -0700

    imagine as a list of magine a list of piles of different frequencies, in increasing order, map to value, map to Frequency

--- leetcode/LFUCache/LFUCache.java
+++ leetcode/LFUCache/LFUCache.java
@@ -27,8 +27,8 @@ cache.get(4);       // returns 4
 idea:
 http://bookshadow.com/weblog/2016/11/22/leetcode-lfu-cache/
 
-key is page address
-value is value
+imagine a list of piles of different frequencies
+the frequency is in an increasing order
 
 head --- FreqNode1 ---- FreqNode2 ---- ... ---- FreqNodeN
               |               |                       |               
@@ -44,115 +44,103 @@ head --- FreqNode1 ---- FreqNode2 ---- ... ---- FreqNodeN
               |
             last
 
-Especially storing all keys with same frequencies in one node,
 LinkedHashSet maintains the insertion order
-
-if one of the keys in that node got hit once more, 
-it will be moved into a new node with (frequency + 1) if the node exits or it will be wrapped into a newly created node with (frequency + 1)
-
+Especially storing all keys with same frequencies in one node
 
 basic structure is doubly linked list of DLLNode, the list is frequency increasing order
 each node, I call it FrequencyNode, beside prev and next for DLLNode, it also has frequency and a LinkedHashset of keys with the same frequency
 valuesMap contains the basic key - value pair
 */
 
-public class LFUCache {
-    private class FrequencyNode {
+class LFUCache {
+    class DLLNode {
+        DLLNode prev;
+        DLLNode next;
         int frequency;
-        FrequencyNode prev = null, next = null;
-        LinkedHashSet<Integer> keys;
+        Set<Integer> keys;
 
-        public FrequencyNode(int frequency) {
-            this.frequency = frequency;
+        public DLLNode(int frequency) {
             this.prev = null;
             this.next = null;
+            this.frequency = frequency;
             this.keys = new LinkedHashSet<Integer>();
         }
     }
-
-    public int capacity;
-    public FrequencyNode head;
-    public HashMap<Integer, Integer> valueHash = null;
-    public HashMap<Integer, FrequencyNode> frequencyHash = null;
-
+    
+    int capacity;
+    DLLNode head;
+    Map<Integer, Integer> valuesMap;
+    Map<Integer, DLLNode> nodesMap;
+    
     public LFUCache(int capacity) {
         this.capacity = capacity;
-        head = null;
-        valueHash = new HashMap<Integer, Integer>();
-        frequencyHash = new HashMap<Integer, FrequencyNode>();
+        this.head = null;
+        this.valuesMap = new HashMap<Integer, Integer>();
+        this.nodesMap = new HashMap<Integer, DLLNode>();
     }
-
+    
     public int get(int key) {
-        if (valueHash.containsKey(key)) {
+        if (valuesMap.containsKey(key)) {
             increaseFrequency(key);
-            return valueHash.get(key);
-        }
-        else {
+            return valuesMap.get(key);
+        } else {
             return -1;
         }
     }
-
+    
     public void put(int key, int value) {
-        if (capacity == 0) {
-            return;
-        }
-        if (!valueHash.containsKey(key)) {
-            // if already full, remove oldest first
-            if (valueHash.size() >= capacity) {
-                // remove the least frequent and least recently used one
+        if (capacity == 0) return;
+        if (!valuesMap.containsKey(key)) {
+            // if full, first remove oldest
+            if (valuesMap.size() >= capacity) {
                 removeOldest();
             }
-            // if not full
-            addToHead(key);
+            addBeforeHead(key);
         }
-        // add to valueHash
-        valueHash.put(key, value);
-        // if ever touched, increase the frequency
+        // update valuesMap
+        valuesMap.put(key, value);
+        // if ever touched, change frequency
         increaseFrequency(key);
     }
-
-    private void increaseFrequency(int key) {
-        FrequencyNode node = frequencyHash.get(key);
-        // since this key's frequency changed, should be removed from LinkedHashSet
+    
+    // increase the frequency of value pointed by key
+    // note, besides increase, remove key from old frequency list
+    public void increaseFrequency(int key) {
+        DLLNode node = nodesMap.get(key);
         node.keys.remove(key);
-        // if node next is null, meaning this is the last node
+        // 3 cases
+        // node is last one, need to create a new node
+        // happen to have a node next to node whose frequency bigger by 1
+        // after increase, have to insert in between of DLL
         if (node.next == null) {
-            FrequencyNode newNode = new FrequencyNode(node.frequency + 1);
+            DLLNode newNode = new DLLNode(node.frequency + 1);
             node.next = newNode;
             newNode.prev = node;
             newNode.keys.add(key);
-        }
-        else if (node.next.frequency == node.frequency + 1) {
+        } else if (node.next.frequency == node.frequency + 1) {
             node.next.keys.add(key);
-        }
-        else { // insert in between
-            FrequencyNode newNode = new FrequencyNode(node.frequency + 1);
+        } else {
+            DLLNode newNode = new DLLNode(node.frequency + 1);
             newNode.keys.add(key);
 
-            FrequencyNode next = node.next;
+            DLLNode next = node.next;
             node.next = newNode;
             newNode.prev = node;
             newNode.next = next;
             next.prev = newNode;
-            // both working
-            // // first new code
-            // newNode.prev = node;
-            // newNode.next = node.next;
-            // // then before and after code
-            // node.next.prev = newNode;
-            // node.next = newNode;
-
         }
-        frequencyHash.put(key, node.next);
-        // frequency node keys size == 0, remove it
-        if ( node.keys.size() == 0 ) {
-            remove(node);
+        // update nodes map
+        nodesMap.put(key, node.next);
+        // old node.keys empty, remove node from DLL
+        if (node.keys.size() == 0) {
+            removeNodeFromDLL(node);
         }
     }
-
-    // the oldest should the first (head) FrequencyNode in the doubly linked list
-    // in the linked hashset last one
-    private void removeOldest() {
+    
+    // the oldest key should be
+    // head in DLL
+    // in head.keys linked hashset, it is the first one added to set
+    public void removeOldest() {
         int oldestKey = 0;
         // the first one
         for (int n : head.keys) {
@@ -164,36 +152,37 @@ public class LFUCache {
         // if keys size() == 0
         // remove the whole keys
         if (head.keys.size() == 0) {
-            remove(head);
+            removeNodeFromDLL(head);
         }
-        valueHash.remove(oldestKey);
-        frequencyHash.remove(oldestKey);
+        valuesMap.remove(oldestKey);
+        nodesMap.remove(oldestKey);
     }
-
-    // add to the basic doubly linked list
-    // case 1, first to add, the linked list is empty
-    // case 2, head.frequency > 1
-    // case 3, directly add to head
-    private void addToHead(int key) {
+    
+    // add node to or before head in DLL
+    // since this is add new key, frequency == 1, to head self or even before head
+    public void addBeforeHead(int key) {
+        // 3 cases:
+        // the first time to add, DLL is empty
+        // add to head
+        // head.frequency > 1, before head
         if (head == null) {
-            head = new FrequencyNode(1);
+            head = new DLLNode(1);
             head.keys.add(key);
-        }
-        else if (head.frequency > 1) {
-            FrequencyNode newNode = new FrequencyNode(1);
+        } else if (head.frequency == 1) {
+            head.keys.add(key);
+        } else {
+            DLLNode newNode = new DLLNode(1);
             newNode.next = head;
             head.prev = newNode;
             head = newNode;
-            newNode.keys.add(key);
-        }
-        else {
             head.keys.add(key);
         }
-        // head is pointing to the new FrequencyNode
-        frequencyHash.put(key, head);
+        // update nodes map
+        nodesMap.put(key, head);
     }
-
-    private void remove(FrequencyNode node) {
+    
+    // remove node from DLL, the scaffold of LFU
+    public void removeNodeFromDLL(DLLNode node) {
         // update node.next and node.prev
         // if node is the head
         if (node.prev == null) {
