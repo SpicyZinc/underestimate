1f2ae443d1569275abefdcf71ecbb2b2f98df515
Spicy Zinc <angxin880@gmail.com>
2017-10-07 15:25:26 -0700

    Google, use prefix[] to keep record of index to start abbreviation for each word in dict, updating it if there are conflicts like two or more words share the same abbreviation, not to forget to add i self; makeAbbr() is having 2nd parameter k, k >= length - 2, return s;

--- leetcode/WordAbbreviation/WordAbbreviation.java
+++ leetcode/WordAbbreviation/WordAbbreviation.java
@@ -19,37 +19,58 @@ The return answers should be in the same order as the original array.
 
 idea:
 record where to start abbreviation, normally abbreviation position starts at 1
-note: lastly add i to set
+note: lastly add i to set, search in [i + 1, size - 1], if set not empty, not forget to add i
 */
 
+import java.util.*;
 public class WordAbbreviation {
+    public static void main(String[] args) {
+        WordAbbreviation eg = new WordAbbreviation();
+        List<String> dict = new ArrayList<String>();
+        dict.add("like");
+        dict.add("god");
+        dict.add("internal");
+        dict.add("me");
+        dict.add("internet");
+        dict.add("interval");
+        dict.add("intension");
+        dict.add("face");
+        dict.add("intrusion");
+
+        for (String abbr : eg.wordsAbbreviation(dict)) {
+            System.out.println(abbr);
+        }
+    }
+
 	public List<String> wordsAbbreviation(List<String> dict) {
         int size = dict.size();
-        String[] ans = new String[size];
+        String[] abbreviations = new String[size];
+        // index to start abbreviation for different word, inclusive
         int[] prefix = new int[size];
         for (int i = 0; i < size; i++) {
             prefix[i] = 1;
-            ans[i] = makeAbbr(dict.get(i), 1);
+            abbreviations[i] = makeAbbr(dict.get(i), 1);
         }
+
         for (int i = 0; i < size; i++) {
             while (true) {
-                HashSet<Integer> set = new HashSet<>();
+                Set<Integer> set = new HashSet<>();
                 for (int j = i + 1; j < size; j++) {
-                	// check all strings with the same abbreviation
-                    if (ans[j].equals(ans[i])) {
+                	// check all strings, find word with the same abbreviation as i
+                    if (abbreviations[j].equals(abbreviations[i])) {
                     	set.add(j);
                     }
                 }
                 if (set.isEmpty()) break;
                 set.add(i);
                 for (int wordPos : set) {
-                	prefix[wordPos] += 1; // increase the prefix
-                    ans[wordPos] = makeAbbr(dict.get(wordPos), prefix[wordPos]); 
+                	prefix[wordPos] += 1; // increase the prefix position
+                    abbreviations[wordPos] = makeAbbr(dict.get(wordPos), prefix[wordPos]); 
                 }
             }
         }
 
-        return Arrays.asList(ans);
+        return Arrays.asList(abbreviations);
     }
 
     private String makeAbbr(String s, int k) {
