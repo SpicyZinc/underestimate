0af57c473bc8cd0594de7d20ca81a7502b99c710
Spicy Zinc <angxin880@gmail.com>
2017-10-10 09:59:27 -0700

    Google, typical recursion in for loop, hashtable key is prefix, value is a list of words with that prefix, all prefix for each word, so prefix can be empty through the whole word as key, based on feature of word squares, try, recursion

--- leetcode/WordSquares/WordSquares.java
+++ leetcode/WordSquares/WordSquares.java
@@ -54,8 +54,63 @@ The output consists of two word squares. The order of output does not matter (ju
 idea:
 
 */
-class WordSquares {
-	public List<List<String>> wordSquares(String[] words) {
 
+import java.util.*;
+
+class WordSquares {
+	public static void main(String[] args) {
+		WordSquares eg = new WordSquares();
+		String[] words = {"area","lead","wall","lady","ball"};
+		List<List<String>> result = eg.wordSquares(words);
+		for (List<String> square : result) {
+			for (String line : square) {
+				System.out.println(line);
+			}
+			System.out.println();
+		}
+	}
+
+	public List<List<String>> wordSquares(String[] words) {
+		List<List<String>> result = new ArrayList<List<String>>();
+		if (words.length == 0 || words[0].length() == 0) {
+			return result;
+		}
+		Map<String, Set<String>> hm = new HashMap<>();
+		int m = words.length;
+		int n = words[0].length();
+		// create all prefix map
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j <= n; j++) {
+				String prefix = words[i].substring(0, j);
+				if (!hm.containsKey(prefix)) {
+					hm.put(prefix, new HashSet<String>());
+				}
+				hm.get(prefix).add(words[i]);
+			}
+		}
+		// dfs
+		dfs(0, n, hm, new ArrayList<String>(), result);
+		return result;
+	}
+
+	public void dfs(int index, int n, Map<String, Set<String>> hm, List<String> path, List<List<String>> result) {
+		if (index == n) {
+			result.add(new ArrayList<String>(path));
+			return;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < index; i++) {
+			sb.append(path.get(i).charAt(index));
+		}
+		Set<String> candidate = hm.get(sb.toString());
+		if (candidate == null) {
+			return;
+		}
+		for (String s : candidate) {
+			path.add(s);
+			dfs(index + 1, n, hm, path, result);
+			path.remove(path.size() - 1);
+		}
 	}
 }
