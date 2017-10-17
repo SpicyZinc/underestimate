3ce3d13ccedb9dda39f7692150e59ee4e5c00d6b
Spicy Zinc <angxin880@gmail.com>
2017-10-16 23:11:09 -0700

    Google, revisited super ugly number, get more clear about it. there is a fact that the ugly number series should be a certain in uglyNumbers multiply corresponding primes[j]; primes[j] multiply uglyNumbers[i], different j has different i associated, should be updated; Shortest distance from all buildings just set up

--- leetcode/SuperUglyNumber/SuperUglyNumber.java
+++ leetcode/SuperUglyNumber/SuperUglyNumber.java
@@ -14,29 +14,40 @@ Note:
 idea:
 https://www.cnblogs.com/Liok3187/p/5016076.html
 http://www.phperz.com/article/15/1225/177142.html
+
+nthSuperUglyNumber(6, [2, 5, 7])
+1.                              [1]                                 min(1 * 2, 1 * 5, 1 * 7)
+2.                           [1, 2]                                 min(1 * 5, 1 * 7, 2 * 2)
+3.                        [1, 2, 4]                                 min(1 * 5, 1 * 7, 4 * 2)
+4.                     [1, 2, 4, 5]                                 min(2 * 5, 1 * 7, 4 * 2)
+5.                  [1, 2, 4, 5, 7]                                 min(2 * 5, 2 * 7, 4 * 2)
 */
 
 public class SuperUglyNumber {
     public int nthSuperUglyNumber(int n, int[] primes) {
         int size = primes.length;
-        int[] positionForEachPrime = new int[size]; // corresponds to the prime at this position reaches what position in the result array
-        int[] res = new int[n];
-        res[0] = 1;
+        // for primes[j] it is time to multiply which ith ugly number
+        // corresponds to the prime at this position shoule multiply which position's ugly number in the result array
+        int[] positionsInResult = new int[size];
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
 
         for (int i = 1; i < n; i++) {
-            int min = Integer.MAX_VALUE;
+            int currMin = Integer.MAX_VALUE;
             for (int j = 0; j < size; j++) {
-                int idxInResult = positionForEachPrime[j]; // index at which the count should multiply the jth prime in primes array 
-                min = Math.min(min, primes[j] * res[idxInResult]);
+                // index at which the count should multiply the jth prime in primes array
+                int posInUglyNumbersTimesPrime = positionsInResult[j];
+                currMin = Math.min(currMin, uglyNumbers[posInUglyNumbersTimesPrime] * primes[j]);
             }
-            // not forget to add to result array
-            res[i] = min;
+            uglyNumbers[i] = currMin;
+            // update positionsInResult[]
             for (int j = 0; j < size; j++) {
-                if (primes[j] * res[positionForEachPrime[j]] == min) {
-                    positionForEachPrime[j]++;
+                if (uglyNumbers[positionsInResult[j]] * primes[j] == currMin) {
+                    positionsInResult[j]++;
                 }
             }
         }
-        return res[n - 1];
+        
+        return uglyNumbers[n - 1];
     }
 }
