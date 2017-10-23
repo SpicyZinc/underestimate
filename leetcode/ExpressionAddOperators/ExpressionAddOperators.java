a1b826d5556dab080c6fb4bd2e4723a68ada4de8
Spicy Zinc <angxin880@gmail.com>
2017-10-23 01:09:02 -0700

    Google, very frequently visited this add opertaions of + - * to expression to get a target, dfs should be used, dfs in for loop

--- leetcode/ExpressionAddOperators/ExpressionAddOperators.java
+++ leetcode/ExpressionAddOperators/ExpressionAddOperators.java
@@ -13,10 +13,9 @@ idea:
 http://blog.csdn.net/u013027996/article/details/48713751
 
 DFS basic idea
-1. overflow: we use a long type once it is larger than Integer.MAX_VALUE or minimum
+1. overflow: use a long type once it is larger than Integer.MAX_VALUE or minimum
 2. 0 sequence: because we can't have numbers with multiple digits started with zero, skip it
 3. a little trick is that we should save the value that is to be multiplied in the next recursion.
-
 */
 
 import java.util.*;
@@ -25,9 +24,8 @@ public class ExpressionAddOperators {
     public static void main(String[] args) {
         ExpressionAddOperators eg = new ExpressionAddOperators();
         List<String> res = eg.addOperators("123", 6);
-
-        for ( int i = 0; i < res.size(); i++ ) {
-            System.out.println(res.get(i));
+        for (String s : res) {
+            System.out.println(s);
         }
     }
 
@@ -36,64 +34,63 @@ public class ExpressionAddOperators {
         dfs(num, target, 0, 0, "", result);
         return result;
     }
-
+    // the net value put on the currRes
     public void dfs(String num, int target, long currRes, long prevRes, String path, List<String> result) {
-        if (currRes == target && num.length() == 0) {
+        if (num.length() == 0 && currRes == target) {
             result.add(new String(path));
             return;
         }
-        
         for (int i = 1; i <= num.length(); i++) {
             String currStr = num.substring(0, i);
-            // exclude any number which has a leading 0
-            // if >= 1, will skip 0, which is a good case 1 * 0 + 5
+            // exclude number of more than 1 digit having leading zero
+            // for 1 digit number and it is zero, no need to exclude, because 1 * 0 + 5 is a good case
             if (currStr.length() > 1 && currStr.charAt(0) == '0') {
                 return;
             }
-            Long currNum = Long.parseLong(currStr);
+            long currNum = Long.parseLong(currStr);
             String nextNum = num.substring(i);
-            // just started dfs for the 1st time
+            // start dfs for the 1st time
             if (path.length() == 0) {
                 dfs(nextNum, target, currNum, currNum, currStr, result);
             } else {
-                // add '*' before currNum
-                dfs(nextNum, target, (currRes - prevRes) + prevRes * currNum, prevRes * currNum, path + "*" + currNum, result);
-                // add '+' before currNum
+                // add * before currNum
+                dfs(nextNum, target, currRes - prevRes + prevRes * currNum, prevRes * currNum, path + "*" + currNum, result);
+                // add + before currNum
                 dfs(nextNum, target, currRes + currNum, currNum, path + "+" + currNum, result);
-                // add '-' before currNum
-                dfs(nextNum, target, currRes - currNum, -currNum, path + "-" + currNum, result);
+                // add - before currNum
+                dfs(nextNum, target, currRes - currNum, -1 * currNum, path + "-" + currNum, result);
             }
         }
     }
 
-    // public List<String> addOperators(String num, int target) {
-    //     List<String> list = new ArrayList<String>();
-    //     dfs(list, num, "", 0, 0, 0, target);
-    //     return list;
-    // }
+    // this one also works
+    public List<String> addOperators(String num, int target) {
+        List<String> list = new ArrayList<String>();
+        dfs(list, num, "", 0, 0, 0, target);
+        return list;
+    }
 
-    // private void dfs(List<String> list, String num, String path, int pos, long sum, long lastNum, int target) {
-    //     int len = num.length();
-    //     if (pos == len && sum == target) {
-    //         list.add(path);
-    //         return;
-    //     }
-    //     if (pos >= len) {
-    //         return;
-    //     }
-    //     for (int i = pos; i < len; i++) {
-    //         if (i != pos && num.charAt(pos) == '0') {
-    //             break;
-    //         }
-    //         long curNum = Long.parseLong(num.substring(pos, i + 1));
-    //         if (pos == 0) {
-    //             dfs(list, num, path + "" + curNum, i + 1, curNum, curNum, target);
-    //         } 
-    //         else {
-    //             dfs(list, num, path + "+" + curNum, i + 1, sum + curNum, curNum, target);
-    //             dfs(list, num, path + "-" + curNum, i + 1, sum - curNum, -curNum, target);
-    //             dfs(list, num, path + "*" + curNum, i + 1, sum - lastNum + lastNum * curNum, lastNum * curNum, target);
-    //         }
-    //     }
-    // }
+    private void dfs(List<String> list, String num, String path, int pos, long sum, long lastNum, int target) {
+        int len = num.length();
+        if (pos == len && sum == target) {
+            list.add(path);
+            return;
+        }
+        if (pos >= len) {
+            return;
+        }
+        for (int i = pos; i < len; i++) {
+            if (i != pos && num.charAt(pos) == '0') {
+                break;
+            }
+            long curNum = Long.parseLong(num.substring(pos, i + 1));
+            if (pos == 0) {
+                dfs(list, num, path + "" + curNum, i + 1, curNum, curNum, target);
+            } else {
+                dfs(list, num, path + "+" + curNum, i + 1, sum + curNum, curNum, target);
+                dfs(list, num, path + "-" + curNum, i + 1, sum - curNum, -curNum, target);
+                dfs(list, num, path + "*" + curNum, i + 1, sum - lastNum + lastNum * curNum, lastNum * curNum, target);
+            }
+        }
+    }
 }
