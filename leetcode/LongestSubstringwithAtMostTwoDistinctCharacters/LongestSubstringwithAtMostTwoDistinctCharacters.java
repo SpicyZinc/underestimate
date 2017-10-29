a744c6b49c95b565a102c20f02e4eb248a385d4f
Spicy Zinc <angxin880@gmail.com>
2017-10-28 23:24:34 -0700

    Google, Longest substring with at most 2 unique characters, hashmap is used to record more than 2 chars appear, sliding window, start++; hm.remove(s.charAt(start)), finally to Math.max(maxLen, s.length() - start);

--- leetcode/LongestSubstringwithAtMostTwoDistinctCharacters/LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ leetcode/LongestSubstringwithAtMostTwoDistinctCharacters/LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -5,10 +5,10 @@ Given S = "eceba",
 T is "ece" which its length is 3.
 
 idea:
-whenever hashmap size > 2, it is time to move the sliding window start position
+whenever hashmap size > 2, it is time to move the sliding window's start position
 calculate the length of any window containing 2 distinct characters,
 then find the max
-the default is that 2 distinct characters will be longer than 1 distinct characters
+the default is that 2 distinct characters will be longer than 1 distinct character
 
 http://blog.csdn.net/whuwangyi/article/details/42451289#
 */
@@ -22,33 +22,33 @@ public class LongestSubstringwithAtMostTwoDistinctCharacters {
 		System.out.println(len);
 	}
 
-    public int lengthOfLongestSubstringTwoDistinct(String s) {
-    	if (s == null || s.length() == 0) {
-    		return 0;
-    	}
+	public int lengthOfLongestSubstringTwoDistinct(String s) {
+		if (s.length() == 0 || s == null) {
+			return 0;
+		}
 
-    	int max = 0;
-    	int start = 0;
-    	HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		Map<Character, Integer> hm = new HashMap<Character, Integer>();
+		int start = 0;
+		int maxLen = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			hm.put(c, hm.getOrDefault(c, 0) + 1);
+			if (hm.size() > 2) {
+				maxLen = Math.max(maxLen, i - start);
+				while (hm.size() > 2) {
+					char charAtStart = s.charAt(start);
+					int cnt = hm.get(charAtStart);
+					if (cnt >= 2) {
+						hm.put(charAtStart, cnt - 1);
+					} else {
+						hm.remove(charAtStart);
+					}
+					start++;
+				}
+			}
+		}
+		maxLen = Math.max(maxLen, s.length() - start);
 
-    	for (int i = 0; i < s.length(); i++) {
-    		char c = s.charAt(i);
-    		hm.put(c, hm.getOrDefault(c, 0) + 1);
-    		if (hm.size() > 2) {
-    			max = Math.max(max, i - start);
-    			while (hm.size() > 2) {
-    				char charAtStart = s.charAt(start);
-    				int cnt = hm.get(charAtStart);
-    				if (cnt >= 2) {
-    					hm.put(charAtStart, cnt - 1);
-    				}
-    				else {
-    					hm.remove(charAtStart);
-    				}
-    				start++;
-    			}
-    		}
-    	}
-    	return Math.max(max, s.length() - start);
-    }
+		return maxLen;
+	}
 }
