fadc32014bd539f346ca9923808c08b201f2248e
Spicy Zinc <angxin880@gmail.com>
2017-10-29 13:51:55 -0700

    Google, trapping water after raining, calculated one trap by one trap, leftHighest and rightHighest not i inclusively, if min(left, right) - height[i] > 0, meaning this trap can trap water, added to the result

--- leetcode/TrappingRainWater/TrappingRainWater.java
+++ leetcode/TrappingRainWater/TrappingRainWater.java
@@ -1,13 +1,15 @@
 /*
 Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
-[0,1,0,2,1,0,1,3,2,1,2,1]
+For example, 
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
 idea:
-calculate one trap by one, in order to do that, need to know the min height around current trap
+calculate one trap by one, in order to do that, need to know the min height to the left and right current trap
 
-i is holding water only if its smaller than min of right max and the left max.
-for one A[i], the maximum water trapped depends on i之前最高的值leftMostHeight[i]和在i右边的最高的值rightMostHeight[i] (exclusively)
-if min(left, right) > A[i], 那么在i这个位置上能trapped的water amount == min(right, left) - A[i]
+trap i is holding water only if its smaller than min of left max and the right max.
+for a height[i], the maximum water trapped depends on leftMostHeight[i] to the left of i
+and rightMostHeight[i] to the right of i (exclusively)
+if min(left, right) > height[i], so trapped water amount on i == min(right, left) - height[i]
 note:
 finally, container[i] == minHeightValue( right, left )
 loop three times
@@ -16,6 +18,36 @@ there is 3rd solution to scan from both ends, like Minimum Window Substring
 http://blog.csdn.net/linhuanmars/article/details/20888505
 */
 public class TrappingRainWater {
+    // recent written, easy to understand
+    public int trap(int[] height) {
+        if (height.length < 3 || height == null) {
+            return 0;
+        }
+        int n = height.length;
+        int[] leftHighest = new int[n];
+        int[] rightHighest = new int[n];
+        int max = 0;
+        // populate lefHighest
+        for (int i = 0; i < n; i++) {
+            leftHighest[i] = max;
+            max = Math.max(max, height[i]);
+        }
+        max = 0;
+        // populate rightHighest
+        for (int i = n - 1; i >= 0; i--) {
+            rightHighest[i] = max;
+            max = Math.max(max, height[i]);
+        }
+        int maxTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            if (Math.min(leftHighest[i], rightHighest[i]) - height[i] > 0) {
+                maxTrapped += Math.min(leftHighest[i], rightHighest[i]) - height[i];
+            }
+        }
+
+        return maxTrapped;
+    }
+
     public int trap(int[] height) {
         if (height == null || height.length < 3) {
             return 0;
@@ -28,7 +60,7 @@ public class TrappingRainWater {
             max = Math.max(max, height[i]);
         }
         max = 0;
-        for (int i = height.length - 1; i >= 0 ; i--) {
+        for (int i = height.length - 1; i >= 0; i--) {
             minHeightValues[i] = Math.min(max, minHeightValues[i]);
             max = Math.max(max, height[i]);
         }
