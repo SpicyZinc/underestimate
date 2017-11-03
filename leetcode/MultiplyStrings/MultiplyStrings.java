5778b474310f591e389d282ff48e923b1b7f9488
Spicy Zinc <angxin880@gmail.com>
2017-11-02 17:26:03 -0700

    Facebook, big integer multiply, basically each digits multiplies each digit, from tail to head, index + 1 === carry, length is n1 + n2

--- leetcode/MultiplyStrings/MultiplyStrings.java
+++ leetcode/MultiplyStrings/MultiplyStrings.java
@@ -1,58 +1,90 @@
 /*
-Multiply Strings
-Given two numbers represented as strings, 
-return multiplication of the numbers as a string.
-Note: The numbers can be arbitrarily large and are non-negative.
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+
+Note:
+The length of both num1 and num2 is < 110.
+Both num1 and num2 contains only digits 0-9.
+Both num1 and num2 does not contain any leading zero.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
 idea:
-1. the length of product of multiplication is at most num1.length + num2.length
-2. not all positions will be used, have to remove the extra positions at last
-3. remember to start from Bit, to ten bit...
-4. store at new array from position 0 to high position
-5. remember to update out loop with carry, the position should be (i + num2.length)
-6. reverse to append
-
+product length will sum of two strings length
+from tail to head, insert to head through tail in product[]
+main idea is single digit multiplies single digit
 */
 public class MultiplyStrings {
 	public static void main(String[] args) {
-		MultiplyStrings aTest = new MultiplyStrings();
-		String s1 = "9";
+		MultiplyStrings eg = new MultiplyStrings();
+		String s1 = "123";
 		String s2 = "9";
 		
-		String product = aTest.multiply(s1, s2);
+		String product = eg.multiply(s1, s2);
 		System.out.println("The product of two nums are " + product);
 	}
-    public String multiply(String num1, String num2) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		
-		// build up product num array first
-		int[] num = new int[num1.length() + num2.length()];
-		
-		for (int i = num1.length()-1; i >= 0; i--) {
+	// best
+	public String multiply(String num1, String num2) {
+        int n1 = num1.length();
+        int n2 = num2.length();
+        int[] product = new int[n1 + n2];
+        
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int a = num1.charAt(i) - '0';
+                int b = num2.charAt(j) - '0';
+                int index = (n1 - i - 1) + (n2 - j - 1);
+                product[index] += a * b;
+                product[index + 1] += product[index] / 10;
+                product[index] %= 10;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = product.length - 1; i > 0; i--) {
+            // get rid of all zeros except i == 0
+            if (sb.length() == 0 && product[i] == 0) {
+                continue;
+            }
+            sb.append(product[i]);
+        }
+        // last one even it is zero, still append
+        sb.append(product[0]);
+        return sb.toString();
+    }
+
+	public String multiply(String num1, String num2) {
+		int n1 = num1.length();
+		int n2 = num2.length();
+		// product array
+		int[] product = new int[n1 + n2];
+
+		for (int i = n1 - 1; i >= 0; i--) {
 			int carry = 0;
-			int a = num1.charAt(i) - '0';
-			for (int j = num2.length()-1; j >= 0; j--) {
+
+			for (int j = n2 - 1; j >= 0; j--) {
+				int a = num1.charAt(i) - '0';
 				int b = num2.charAt(j) - '0';
-				num[(num1.length()-1-i) + (num2.length()-1-j)] += (carry + a * b);
-				carry = num[(num1.length()-1-i) + (num2.length()-1-j)] / 10;
-				num[(num1.length()-1-i) + (num2.length()-1-j)] %= 10;
+
+				int index = (n1 - 1 - i) + (n2 - 1 - j);
+
+				product[index] += (carry + a * b);
+				carry = product[index] / 10;
+				product[index] %= 10;
 			}
 			// do not forget to update 
-			num[(num1.length()-1-i) + num2.length()] += carry;			
+			product[(n1 - 1 - i) + n2] += carry;
 		}
 		// find where is not-zero position
 		// ATTENTION: i>0 not i>=0, use while(), be careful
-		int i = num.length - 1;
-		while (i > 0 && num[i] == 0) {
+		int i = product.length - 1;
+		while (i > 0 && product[i] == 0) {
 			i--;
 		}
 		// result
-		StringBuilder res = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		for (int k = i; k >= 0; k--) {
-			res.append( (char)(num[k] + '0') );
+			sb.append((char) (product[k] + '0'));
 		}
 		
-		return res.toString();
-    }
+		return sb.toString();
+	}
 }
