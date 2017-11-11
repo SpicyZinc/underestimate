02ce57e693864654f059ec1be5451d326791acd8
Spicy Zinc <angxin880@gmail.com>
2017-11-11 02:28:49 -0800

    new problem leetcode, simple, how come not thinking of answer, set to keep the built path to the longest

---
+++ leetcode/LongestWordInDictionary/LongestWordInDictionary.java
@@ -0,0 +1,47 @@
+/*
+Given a list of strings words representing an English Dictionary, find the longest word in words that can be built one character at a time by other words in words.
+If there is more than one possible answer, return the longest word with the smallest lexicographical order.
+If there is no answer, return the empty string.
+
+Example 1:
+Input: 
+words = ["w","wo","wor","worl", "world"]
+Output: "world"
+Explanation: 
+The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+
+Example 2:
+Input: 
+words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+Output: "apple"
+Explanation: 
+Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
+
+Note:
+All the strings in the input will only contain lowercase letters.
+The length of words will be in the range [1, 1000].
+The length of words[i] will be in the range [1, 30].
+
+idea:
+simple, but I did not think of
+first sort array of strings
+Set<String> builtPath to keep record of ALL possible words to the longest
+*/
+
+class LongestWordInDictionary {
+	public String longestWord(String[] words) {
+		String longest = "";
+		Arrays.sort(words);
+		// all words path to the longest
+		Set<String> builtPath = new HashSet<String>();
+		for (String word : words) {
+			String prev = word.substring(0, word.length() - 1);
+			if (word.length() == 1 || builtPath.contains(prev)) {
+				builtPath.add(word);
+				// once update, next word, same length, would NOT update, but it will update the longest, we want alphabetically smaller one
+				longest = word.length() > longest.length() ? word : longest;
+			}
+		}
+		return longest;
+	}
+}
