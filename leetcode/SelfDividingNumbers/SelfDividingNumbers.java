42423a211da206fec1c5a67a1529225a0e9b9506
Spicy Zinc <angxin880@gmail.com>
2017-11-28 00:35:05 -0800

    similarity is to use hash map string to set, pair[0] and pair[1]; self dividing number is just follow the problem, straightforward

---
+++ leetcode/SelfDividingNumbers/SelfDividingNumbers.java
@@ -0,0 +1,49 @@
+/*
+A self-dividing number is a number that is divisible by every digit it contains.
+For example, 128 is a self-dividing number
+because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+Also, a self-dividing number is not allowed to contain the digit zero.
+Given a lower and upper number bound, output a list of every possible self dividing number,
+including the bounds if possible.
+
+Example 1:
+Input: 
+left = 1, right = 22
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+
+Note:
+The boundaries of each input argument are 1 <= left <= right <= 10000.
+
+idea:
+helper function to detect a number if self dividing number
+then loop from [left, right], if dividing, push to the result list
+*/
+
+class SelfDividingNumbers {
+	public List<Integer> selfDividingNumbers(int left, int right) {
+		List<Integer> result = new ArrayList<Integer>();
+		for (int i = left; i <= right; i++) {
+			if ( isSelfDividing(i) ) {
+				result.add(i);
+			}
+		}
+
+		return result;
+	}
+
+	public boolean isSelfDividing(int num) {
+		int number = num;
+		
+		while (number > 0) {
+			int digit = number % 10;
+			// 0 can not be denominator
+			if (digit == 0 || num % digit != 0) {
+				return false;
+			}
+			number /= 10;
+		}
+
+		return number == 0;
+	}
+}
