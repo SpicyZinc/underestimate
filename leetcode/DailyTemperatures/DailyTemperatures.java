2b39bfb328fdc2a06add3ba0ea7c02e3f50ffd0e
Spicy Zinc <angxin880@gmail.com>
2017-12-09 16:49:32 -0800

    direct 2 for loops, easy to figure it out

---
+++ leetcode/DailyTemperatures/DailyTemperatures.java
@@ -0,0 +1,31 @@
+/*
+Given a list of daily temperatures, produce a list that,
+for each day in the input, tells you how many days you would have to wait until a warmer temperature.
+If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+idea:
+direct 2 for loops, no more need to say
+*/
+
+class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] warmers = new int[len];
+        for (int i = 0; i < temperatures.length; i++) {
+            int tem = temperatures[i];
+            int days = 0;
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[j] > tem) {
+                    days = j - i;
+                    break;
+                }
+            }
+            warmers[i] = days;
+        }
+        
+        return warmers;
+    }
+}
