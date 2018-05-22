246fd4f5010cb31301023a83bac3d34e9e0bb100
Spicy Zinc <angxin880@gmail.com>
2018-05-22 10:47:19 -0700

    Push all google problems to remote

--- leetcode/PermutationsII/PermutationsII.java
+++ leetcode/PermutationsII/PermutationsII.java
@@ -8,69 +8,30 @@ For example,
 idea: for those duplicates, only count 1st unique one, skip the rest duplicates
 */
 public class PermutationsII {
-	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		permuteUnique(num, 0, result);
-		return result;
-	}
-	 
-	private void permuteUnique(int[] num, int start, ArrayList<ArrayList<Integer>> result) {
-		if (start == num.length - 1) {
-			ArrayList<Integer> temp = new ArrayList<Integer>();
-			for (int x : num) {
-				temp.add(x);
-			}
-
-			result.add( temp );
-		}
-		else {
-			for (int j = start; j < num.length; j++) {
-				if ( !containsDuplicate(num, start, j) ) {
-					swap(num, start, j);
-					permuteUnique(num, start + 1, result);
-					swap(num, start, j);
-				}
-			}
-		}
-	}
-	 
-	private boolean containsDuplicate(int[] arr, int start, int end) {
-		for (int i = start; i < end; i++) {
-			if (arr[i] == arr[end]) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-	 
-	private void swap(int[] a, int i, int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
-
-
-
 	public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums==null || nums.length==0) { return ans; }
+        if (nums == null || nums.length == 0) {
+			return ans;
+        }
         permute(ans, nums, 0);
+
         return ans;
     }
     
     private void permute(List<List<Integer>> ans, int[] nums, int index) {
         if (index == nums.length) { 
             List<Integer> temp = new ArrayList<>();
-            for (int num: nums) { temp.add(num); }
+            for (int num: nums) {
+				temp.add(num);
+            }
             ans.add(temp);
             return;
         }
         Set<Integer> appeared = new HashSet<>();
-        for (int i=index; i<nums.length; ++i) {
+        for (int i = index; i < nums.length; i++) {
             if (appeared.add(nums[i])) {
                 swap(nums, index, i);
-                permute(ans, nums, index+1);
+                permute(ans, nums, index + 1);
                 swap(nums, index, i);
             }
         }
