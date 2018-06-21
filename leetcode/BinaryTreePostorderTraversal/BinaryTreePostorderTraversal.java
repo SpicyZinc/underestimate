2986dc090997e2df32df3d3a8551858e4a265aa1
Spicy Zinc <angxin880@gmail.com>
2018-06-21 01:26:27 -0700

    post order traversal draw triangle to memorize, stack is hard to remember

--- leetcode/BinaryTreePostorderTraversal/BinaryTreePostorderTraversal.java
+++ leetcode/BinaryTreePostorderTraversal/BinaryTreePostorderTraversal.java
@@ -1,7 +1,20 @@
 /*
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+Output: [3,2,1]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
 idea:
 http://blog.csdn.net/ljphhj/article/details/21369053
 */
+
 class TreeNode {
     int val;
     TreeNode left;
