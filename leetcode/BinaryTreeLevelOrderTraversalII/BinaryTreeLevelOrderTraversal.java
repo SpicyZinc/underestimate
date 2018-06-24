47fcfd27f5a91a6596304744b18d5dd492577487
Spicy Zinc <angxin880@gmail.com>
2018-06-23 20:12:15 -0700

    Collections.reverse() is key

---
+++ leetcode/BinaryTreeLevelOrderTraversalII/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,63 @@
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+
+idea:
+the difference from 1st one is to Collections.sort();
+*/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class BinaryTreeLevelOrderTraversal {
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+
+		if (root == null) {
+			return result;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			List<Integer> layer = new ArrayList<Integer>();
+			int size = queue.size();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+
+				layer.add(node.val);
+
+				if (node.left != null) {
+					queue.add(node.left);
+				}
+
+				if (node.right != null) {
+					queue.add(node.right);
+				}
+			}
+			result.add(layer);
+		}
+		Collections.reverse(result);
+
+		return result;
+	}
+}
