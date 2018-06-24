8b77cfcb4016f33238816af1188d3404ba2bdb74
Spicy Zinc <angxin880@gmail.com>
2018-06-23 17:42:19 -0700

    Google, beautiful arrangement I typical dfs in for loop and visited array

--- leetcode/BeautifulArrangement/BeautifulArrangement.java
+++ leetcode/BeautifulArrangement/BeautifulArrangement.java
@@ -1,9 +1,10 @@
 /*
 Suppose you have N integers from 1 to N.
-We define a beautiful arrangement as an array that is constructed by these N numbers successfully if one of the following is true for the ith position (1 ≤ i ≤ N) in this array:
-
+We define a beautiful arrangement as an array that is constructed by these N numbers successfully
+if one of the following is true for the ith position (1 ≤ i ≤ N) in this array:
 The number at the ith position is divisible by i.
 i is divisible by the number at the ith position.
+
 Now given N, how many beautiful arrangements can you construct?
 
 Example 1:
@@ -30,54 +31,30 @@ N * (N-1) * (N-2) * ... * 1
 use visited to mark if one number is used before
 for each position, for loop from 1 to N
 how to implement it, dfs or backtracking
+typical dfs in for loop and visited array to help
 */
 
 public class BeautifulArrangement {
-    int count = 0;
-    
-    public int countArrangement(int N) {
-        if (N == 0) {
-            return 0;
-        }
-        helper(N, 1, new int[N + 1]);
-        return count;
+	public int countArrangement(int N) {
+        int[] count = new int[1];
+        boolean[] visited = new boolean[N];
+        dfs(N, 0, count, visited);
+        
+        return count[0];
     }
     
-    private void helper(int N, int pos, int[] visited) {
-        if (pos > N) {
-            count++;
+    public void dfs(int N, int pos, int[] count, boolean[] visited) {
+        // found a beautiful arrangement
+        if (pos == N) {
+            count[0]++;
             return;
         }
         
-        for (int i = 1; i <= N; i++) {
-            if (visited[i] == 0 && (i % pos == 0 || pos % i == 0)) {
-                visited[i] = 1;
-                helper(N, pos + 1, visited);
-                visited[i] = 0;
-            }
-        }
-    }
-
-    // self written
-    int count = 0;
-
-    public int countArrangement(int N) {
-        if (N <= 1) {
-            return N;
-        }
-        boolean[] visited = new boolean[N];
-        helper(N, 0, visited);
-        return count;
-    }
-    
-    private void helper(int N, int pos, boolean[] visited) {
-        if (pos == N) {
-            count++;
-        }
         for (int i = 0; i < N; i++) {
-            if ( !visited[i] && ( (i+1) % (pos+1) == 0 || (pos+1) % (i+1) == 0 ) ) {
+            boolean divisible = (i + 1) % (pos + 1) == 0 || (pos + 1) % (i + 1) == 0;
+            if (!visited[i] && divisible) {
                 visited[i] = true;
-                helper(N, pos + 1, visited);
+                dfs(N, pos + 1, count, visited);
                 visited[i] = false;
             }
         }
