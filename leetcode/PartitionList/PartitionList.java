9ab5c42ebab3516ecc80ff20197aa445582033f8
Spicy Zinc <angxin880@gmail.com>
2018-06-28 07:08:56 -0700

    partition list, two new lists, connect them together at last

--- leetcode/PartitionList/PartitionList.java
+++ leetcode/PartitionList/PartitionList.java
@@ -1,6 +1,4 @@
 /*
-Partition List
-
 Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 original relative order of the nodes in each of the two partitions should be reserved. 
 
