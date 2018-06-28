3cfabfe889ef00891e87586086363d8d419bb77d
Spicy Zinc <angxin880@gmail.com>
2018-06-28 04:42:14 -0700

    union find good example, ids[root] = root, tree, not ge zi wei zheng, roots[index] == rootIdx to union

--- leetcode/NumberOfIslandsII/NumberOfIslands.java
+++ leetcode/NumberOfIslandsII/NumberOfIslands.java
@@ -5,11 +5,41 @@ Given a list of positions to operate, count the number of islands after each add
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
 You may assume all four edges of the grid are all surrounded by water.
 
-idea:
-union-find
-note: roots[roots[roots[roots[c]]]] ... until roots[c] = c;
+Example:
 
-https://discuss.leetcode.com/topic/29613/easiest-java-solution-with-explanations/2
+Given m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]].
+Initially, the 2d grid grid is filled with water. (Assume 0 represents water and 1 represents land).
+0 0 0
+0 0 0
+0 0 0
+
+Operation #1: addLand(0, 0) turns the water at grid[0][0] into a land.
+1 0 0
+0 0 0   Number of islands = 1
+0 0 0
+
+Operation #2: addLand(0, 1) turns the water at grid[0][1] into a land.
+1 1 0
+0 0 0   Number of islands = 1
+0 0 0
+
+Operation #3: addLand(1, 2) turns the water at grid[1][2] into a land.
+1 1 0
+0 0 1   Number of islands = 2
+0 0 0
+
+Operation #4: addLand(2, 1) turns the water at grid[2][1] into a land.
+1 1 0
+0 0 1   Number of islands = 3
+0 1 0
+
+We return the result as an array: [1, 1, 2, 3]
+
+idea:
+good article about union-find, https://blog.csdn.net/dm_vincent/article/details/7655764
+note:
+ids[ids[ids[ids[root]]]] ... till ids[root] = root
+底层仍然是[] 但是是 tree 结构
 */
 
 import java.util.*;
@@ -19,7 +49,8 @@ class NumberOfIslands {
 		NumberOfIslands eg = new NumberOfIslands();
 		int m = 3;
 		int n = 3;
-		int[][] positions = {{0,0}, {0,2}, {0,1}, {1,1}};
+		// int[][] positions = {{0,0}, {0,2}, {0,1}, {1,1}};
+		int[][] positions = {{0,0}, {0,1}, {1,2}, {2,1}};
 		List<Integer> result = eg.numIslands2(m,n,positions);
 		System.out.println(result);
 	}
@@ -31,32 +62,33 @@ class NumberOfIslands {
 		Arrays.fill(roots, -1);
 
 		int[][] directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-		int cnt = 0;
+		int islandCnt = 0;
 
 		for (int[] position : positions) {
-			cnt++;
+			// add one more land, assume it is not connected, one more island
+			islandCnt++;
 
 			int x = position[0];
 			int y = position[1];
-			int insertIndex = x * n + y;
+			int index = x * n + y;
 			// itself is itself's parent
-			roots[insertIndex] = insertIndex;
+			roots[index] = index;
 
 			for (int[] dir : directions) {
 				int nextX = x + dir[0];
 				int nextY = y + dir[1];
-				int neighborIndex = nextX * n + nextY;
-				// roots[neighborIndex] != -1 means it is land, visited before, now needs to update
-				if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && roots[neighborIndex] != -1) {
-					int rootIdx = getRootIdx(roots, neighborIndex);
+				int nextIndex = nextX * n + nextY;
+				// roots[nextIndex] != -1 means it is land, visited before, now needs to update
+				if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && roots[nextIndex] != -1) {
+					int rootIdx = getRootIdx(roots, nextIndex);
 					// should equal, why because it is current position's 4 neighbors,
 					// belonging to the same island, should have the same parent
 					// if not, need to update
-					// always use the i == roots[i] in roots to update the current insertIndex's parent root
-					if (insertIndex != rootIdx) {
-						roots[insertIndex] = rootIdx; // insertIndex's parent is rootIdx
-						insertIndex = rootIdx;
-						cnt--;
+					// always use the i == roots[i] in roots to update the current index's parent root
+					if (index != rootIdx) {
+						roots[index] = rootIdx; // index's parent is rootIdx
+						index = rootIdx;
+						islandCnt--;
 					}
 				}
 			}
@@ -66,7 +98,7 @@ class NumberOfIslands {
 			}
 			System.out.println();
 
-			result.add(cnt);
+			result.add(islandCnt);
 		}
 
 		return result;
