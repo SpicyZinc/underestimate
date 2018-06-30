c6b165059040e1d4f189416d0c73076c205da383
Spicy Zinc <angxin880@gmail.com>
2018-06-30 16:17:30 -0700

    Google, last one top 100, wave sort

--- leetcode/WiggleSort/WiggleSort.java
+++ leetcode/WiggleSort/WiggleSort.java
@@ -3,14 +3,15 @@ Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >
 For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
 
 idea:
+摆动排序
 https://segmentfault.com/a/1190000003783283
 
 this is wiggle sort I, allowing equal sign
 
 1. sort first, start from 3rd element, swap 3rd with 2nd, swap 5th with 4th
 2. O(n), just swap()
-i % 2 == 1 nums[i] >= nums[i - 1]
-i % 2 == 0 nums[i] <= nums[i - 1]
+i % 2 == 1 must nums[i] >= nums[i - 1]
+i % 2 == 0 must nums[i] <= nums[i - 1]
 */
 
 import java.util.*;
@@ -28,15 +29,18 @@ public class WiggleSort {
 	}
 
 	// method 1
-    // public void wiggleSort(int[] nums) {
-    //     if (nums.length <= 1 || nums == null) {
-    //         return;   
-    //     }
-    //     Arrays.sort(nums);
-    //     for (int i = 2; i < nums.length; i += 2) {
-    //     	swap(nums, i - 1, i);
-    //     }
-    // }
+    public void wiggleSort(int[] nums) {
+        if (nums.length == 0 || nums == null) {
+            return;
+        }
+        
+        Arrays.sort(nums);
+        for (int i = 2; i < nums.length; i += 2) {
+            int temp = nums[i - 1];
+            nums[i - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
 
     // method 2
     public void wiggleSort(int[] nums) {
@@ -45,7 +49,7 @@ public class WiggleSort {
         }
         for (int i = 1; i < nums.length; i++) {
         	if ( (i % 2 == 1 && nums[i] < nums[i - 1]) || (i % 2 == 0 && nums[i] > nums[i - 1]) ) {
-        		swap(nums, i-1, i);
+        		swap(nums, i - 1, i);
         	}
         }
     }
