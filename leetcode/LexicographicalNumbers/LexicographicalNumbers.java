cbf2cbc8883bb59291d7d143f2aae801a38fa721
Spicy Zinc <angxin880@gmail.com>
2018-07-02 05:29:14 -0700

    iterative method 3 cased easy to understand, note the last case

--- leetcode/LexicographicalNumbers/LexicographicalNumbers.java
+++ leetcode/LexicographicalNumbers/LexicographicalNumbers.java
@@ -73,7 +73,17 @@ the result should be
 2
 20
 21
-22 
+22
+
+idea:
+https://blog.csdn.net/Cloudox_/article/details/70224397
+
+1 如果一个数乘以十以后没有超过n, 那它后面紧挨着的应该是它的十倍, 比如1,10,100.
+2 如果不满足1, 那就应该是直接加一, 比如n为13的时候, 前一个数为12, 120超过了n, 那接着的应该是13.
+但是这里要注意如果前一个数的个位已经是9或者是它就是n了, 那就不能加一了, 比如 n = 25, 前一个数为19, 下一个数应该为2而不是19+1=20.
+25的下一个也没有26了.
+3 如果不满足2, 比如19后面应该接2而不是20, 这时候应该将19除以10再加一, 比如n=500, 399的下一个应该是4, 那就是除以十, 个位还是9, 继续除以10, 得到3, 加一得到4.
+
 */
 import java.util.*;
 
@@ -84,6 +94,34 @@ public class LexicographicalNumbers {
         for (int i = 0; i < result.size(); i++) {
             System.out.println(result.get(i));
         }
+    }
+
+    // iteration
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<Integer>();
+        // current stands for the number sequentially inserted to result array
+        int current = 1;
+        // after all, n 个数是不变的
+        for (int i = 0; i < n; i++) {
+            result.add(current);
+
+            if (current * 10 <= n) {
+                current *= 10;
+            } else if (current % 10 != 9 && current + 1 <= n) {
+                current += 1;
+            } else {
+                // 找到不是9的 digit
+                // n = 20
+                // 记住多了一步/10, 实际是倒数第二位
+                while ((current / 10) % 10 == 9) {
+                    current /= 10;
+                }
+                current = current / 10 + 1;
+                
+            }
+        }
+
+        return result;
     }
 
     public List<Integer> lexicalOrder(int n) {
