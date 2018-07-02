41bc0d77df236f0bc9d240fadf379ad10f28dc11
Spicy Zinc <angxin880@gmail.com>
2018-07-01 18:07:40 -0700

    only one black pixel area, like number of islands one island, updating bottom left and top right x and y, or dfs not visited

--- leetcode/SmallestRectangleEnclosingBlackPixels/SmallestRectangleEnclosingBlackPixels.java
+++ leetcode/SmallestRectangleEnclosingBlackPixels/SmallestRectangleEnclosingBlackPixels.java
@@ -13,22 +13,67 @@ and x = 0, y = 2,
 Return 6.
 
 idea:
+use bottom left and top right to represent rectangle
 dfs
 even better dfs with binary search
 https://discuss.leetcode.com/topic/30621/1ms-concise-java-binary-search-dfs-is-4ms
 */
 public class SmallestRectangleEnclosingBlackPixels {
-	private int blX;
-	private int blY;
-	private int trX;
-	private int trY;
+	public static void main(String[] args) {
+		char[][] image = {
+			{'0', '0', '1', '0',},
+			{'0', '1', '1', '0',},
+			{'0', '1', '0', '0',},
+		};
+
+		int x = 0;
+		int y = 2;
+
+		SmallestRectangleEnclosingBlackPixels eg = new SmallestRectangleEnclosingBlackPixels();
+		int minArea = eg.minArea(image, x, y);
+		System.out.println(minArea);
+	}
+
+	// method 1
+	// 题目说了 only one black pixel area
+	// 所以遇到 1 就更新 各个端点坐标就行
+	public int minArea(char[][] image, int x, int y) {
+		int btmLeftX = x;
+		int btmLeftY = y;
+		int topRightX = x;
+		int topRightY = y;
+		int area = 0;
+
+		int m = image.length;
+		int n = image[0].length;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (image[i][j] == '1') {
+					btmLeftX = Math.min(btmLeftX, i);
+					btmLeftY = Math.min(btmLeftY, j);
+					topRightX = Math.max(topRightX, i);
+					topRightY = Math.max(topRightY, j);
+
+					area = Math.max(area, (topRightX - btmLeftX + 1) * (topRightY - btmLeftY + 1));
+				}
+			}
+		}
+
+		return area;
+	}
+
+	private int btmLeftX;
+	private int btmLeftY;
+	private int topRightX;
+	private int topRightY;
 	private int area;
 
 	public int minArea(char[][] image, int x, int y) {
-		this.blX = x;
-		this.blY = y;
-		this.trX = x;
-		this.trY = y;
+		this.btmLeftX = x;
+		this.btmLeftY = y;
+		this.topRightX = x;
+		this.topRightY = y;
 		this.area = 0;
 
 		if (image.length == 0 || image == null) {
@@ -56,40 +101,21 @@ public class SmallestRectangleEnclosingBlackPixels {
 		int m = image.length;
 		int n = image[0].length;
 
-		if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) {
+		if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || image[x][y] == '0') {
 			return;
 		}
-		if (image[x][y] == '0') {
-			return;
-		}
-		// set this (x,y) to be true
-		visited[x][y] = true;
-		// update bl and tr
-		blX = Math.min(blX, x);
-		blY = Math.min(blY, y);
-		trX = Math.max(trX, x);
-		trY = Math.max(trY, y);
 
-		area = Math.max(area, (trX - blX + 1) * (trY - blY + 1));
-		// 4 directions
+		// mark this (x,y) visited
+		visited[x][y] = true;
+		// 不断扩大 rectangle 范围
+		btmLeftX = Math.min(btmLeftX, x);
+		btmLeftY = Math.min(btmLeftY, y);
+		topRightX = Math.max(topRightX, x);
+		topRightY = Math.max(topRightY, y);
+		area = Math.max(area, (topRightX - btmLeftX + 1) * (topRightY - btmLeftY + 1));
+
 		for (int[] dir : directions) {
 			dfs(image, visited, x + dir[0], y + dir[1]);
 		}
-	}
-
-	public static void main(String[] args) {
-		char[][] image = {
-			{'0', '0', '1', '0',},
-			{'0', '1', '1', '0',},
-			{'0', '1', '0', '0',},
-		};
-
-		int x = 0;
-		int y = 2;
-		
-
-		SmallestRectangleEnclosingBlackPixels eg = new SmallestRectangleEnclosingBlackPixels();
-		int minArea = eg.minArea(image, x, y);
-		System.out.println(minArea);
 	}
 }
