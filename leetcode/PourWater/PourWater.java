89d51996ce700735335e0de8f4aaf7249a6726ed
Spicy Zinc <angxin880@gmail.com>
2018-07-02 03:06:42 -0700

    drop water, each droplet each droplet , find monotolic decreasing , dropStopPos = i, heights[dropStopPos]++

--- leetcode/PourWater/PourWater.java
+++ leetcode/PourWater/PourWater.java
@@ -7,8 +7,10 @@ Water first drops at index K and rests on top of the highest terrain or water at
 If the droplet would eventually fall by moving left, then move left.
 Otherwise, if the droplet would eventually fall by moving right, then move right.
 Otherwise, rise at it's current position.
-Here, "eventually fall" means that the droplet will eventually be at a lower level if it moves in that direction. Also, "level" means the height of the terrain plus any water in that column.
-We can assume there's infinitely high terrain on the two sides out of bounds of the array. Also, there could not be partial water being spread out evenly on more than 1 grid block - each unit of water has to be in exactly one block.
+Here, "eventually fall" means that the droplet will eventually be at a lower level if it moves in that direction.
+Also, "level" means the height of the terrain plus any water in that column.
+We can assume there's infinitely high terrain on the two sides out of bounds of the array.
+Also, there could not be partial water being spread out evenly on more than 1 grid block - each unit of water has to be in exactly one block.
 
 Example 1:
 Input: heights = [2,1,1,2,1,2,2], V = 4, K = 3
@@ -111,7 +113,8 @@ V will be in range [0, 2000].
 K will be in range [0, heights.length - 1].
 
 idea:
-
+take a reference to huahua
+https://www.cnblogs.com/grandyang/p/8460541.html
 */
 
 class PourWater {
@@ -123,19 +126,22 @@ class PourWater {
 	}
 
 	private void dropWater(int[] heights, int K) {
-		int best = K;
+		int dropStopPos = K;
+		// 先往左 再往右 trick 统一起来
 		for (int d = -1; d <= 1; d += 2) {
 			int i = K + d;
+			// monotonically decreasing => heights[i] <= heights[i - d]
+			// non-increasing
 			while (i >= 0 && i < heights.length && heights[i] <= heights[i - d]) {
-				if (heights[i] < heights[best]) {
-					best = i;
+				if (heights[i] <= heights[dropStopPos]) {
+					dropStopPos = i;
 				}
 				i += d;
 			}
-			if (best != K) {
+			if (dropStopPos != K) {
 				break;
 			}
 		}
-		heights[best]++;
+		heights[dropStopPos]++;
 	}
 }
