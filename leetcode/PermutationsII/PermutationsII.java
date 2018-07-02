2b1e9a97b3f7d7d235656c401bc0dbaae06db9bd
Spicy Zinc <angxin880@gmail.com>
2018-07-02 02:19:39 -0700

    permute with duplicates, dfs() hashset, swap, dfs(), swap back, got to remember

--- leetcode/PermutationsII/PermutationsII.java
+++ leetcode/PermutationsII/PermutationsII.java
@@ -6,40 +6,45 @@ For example,
 [1,1,2], [1,2,1], and [2,1,1].
 
 idea: for those duplicates, only count 1st unique one, skip the rest duplicates
+typical dfs() with hashset to remove duplicates
 */
 public class PermutationsII {
 	public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-			return ans;
-        }
-        permute(ans, nums, 0);
+		List<List<Integer>> result = new ArrayList<>();
 
-        return ans;
-    }
-    
-    private void permute(List<List<Integer>> ans, int[] nums, int index) {
-        if (index == nums.length) { 
-            List<Integer> temp = new ArrayList<>();
-            for (int num: nums) {
-				temp.add(num);
-            }
-            ans.add(temp);
-            return;
-        }
-        Set<Integer> appeared = new HashSet<>();
-        for (int i = index; i < nums.length; i++) {
-            if (appeared.add(nums[i])) {
-                swap(nums, index, i);
-                permute(ans, nums, index + 1);
-                swap(nums, index, i);
-            }
-        }
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int save = nums[i];
-        nums[i] = nums[j];
-        nums[j] = save;
-    }
+		if (nums == null || nums.length == 0) {
+			return result;
+		}
+
+		permute(nums, 0, result);
+		
+		return result;
+	}
+
+	// dfs permute()
+	private void permute(int[] nums, int pos, List<List<Integer>> result) {
+		if (pos == nums.length) {
+			List<Integer> path = new ArrayList<Integer>();
+			for (int num : nums) {
+				path.add(num);
+			}
+			result.add(path);
+			return;
+		}
+		
+		Set<Integer> used = new HashSet<Integer>();
+		for (int i = pos; i < nums.length; i++) {
+			if (used.add(nums[i])) {
+				swap(nums, pos, i);
+				permute(nums, pos + 1, result);
+				swap(nums, pos, i);
+			}
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int save = nums[i];
+		nums[i] = nums[j];
+		nums[j] = save;
+	}
 }
