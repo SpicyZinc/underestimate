6055e81d21e43340635cc85baea04d0172d0bdfe
Spicy Zinc <angxin880@gmail.com>
2018-08-29 10:29:19 -0700

    Amazon, medium, restore ip address, similiar to ways to decode 26 letters

--- leetcode/RestoreIPAddresses/RestoreIPAddresses.java
+++ leetcode/RestoreIPAddresses/RestoreIPAddresses.java
@@ -1,44 +1,77 @@
 /*
-Restore IP Addresses
-Given a string containing only digits,
-restore it by returning all possible valid IP address combinations.
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
 
-Given "25525511135",
-return ["255.255.11.135", "255.255.111.35"]. (Order does not matter) 
+Example:
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]
 
 idea:
-iterative, brute force, find all possibilities, get rid of invalid ip address
+1. dfs
+2. iterative, brute force, find all possibilities, get rid of invalid ip address
 helper method isValidIp() to help judge if it is a valid ip
-==
-recursion, do not understand
-
 */
 
 import java.util.*;
 
 public class RestoreIPAddresses {
-    public ArrayList<String> restoreIpAddresses(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-		ArrayList<String> res = new ArrayList<String>();        
+    // dfs
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<String>();
+
+        if (s == null || s.length() < 4 || s.length() > 12) {
+            return result;
+        }
+
+        dfs(s, 0, "", result);
+        return result;
+    }
+
+    public void dfs(String s, int count, String path, List<String> result) {
+        if (count == 3 && isValidIP(s)) {
+            result.add(path + s);
+
+            return;
+        }
+
+        for (int i = 1; i < 4 && i < s.length(); i++) {
+            // 1 of 4 parts in xxx.xxx.xxx.xxx
+            String part = s.substring(0, i);
+            if (isValidIP(part)) {
+                dfs(s.substring(i), count + 1, path + part + ".", result);
+            }
+        }
+    }
+
+    private boolean isValidIP(String s) {
+        if (s.charAt(0) == '0') {
+            return s.equals("0");
+        }
+
+        int num = Integer.parseInt(s);
+        return num > 0 && num <= 255;
+    }
+
+    // iteration
+    public List<String> restoreIpAddresses(String s) {
+		List<String> result = new ArrayList<String>();        
         if (s.length() > 12 || s.length() < 4) {
-            return res;
-        } 
-        int n = s.length();
- 
+            return result;
+        }
+
+        int n = s.length(); 
         int i = 0;
         for (int j = i+1; j < n; j++) {
             for (int k = j+1; k < n; k++) {
                 for (int l = k+1; l < n; l++) {
                     String a = s.substring(i, j), b = s.substring(j, k), c = s.substring(k, l), d = s.substring(l);
                     if (isValidIp(a, b, c, d)) {
-                        res.add(a + "." + b + "." + c + "." + d);
+                        result.add(a + "." + b + "." + c + "." + d);
                     }
                 }
             }
         }
  
-        return res;
+        return result;
     }
  
     public boolean isValidIp(String a, String b, String c, String d) {
@@ -48,9 +81,9 @@ public class RestoreIPAddresses {
         }
  
         boolean zVal = (a.length() == 1 || a.charAt(0) != '0') && 
-						(b.length() == 1 || b.charAt(0) != '0') &&
-						(c.length() == 1 || c.charAt(0) != '0') && 
-						(d.length() == 1 || d.charAt(0) != '0');
+			(b.length() == 1 || b.charAt(0) != '0') &&
+			(c.length() == 1 || c.charAt(0) != '0') && 
+			(d.length() == 1 || d.charAt(0) != '0');
  
         if (!zVal) {
             return false;
@@ -68,86 +101,6 @@ public class RestoreIPAddresses {
 		boolean vVal = aVBool && bVBool && cVBool && dVBool;
 		
         // boolean vVal = (aV >> 8) == 0 && (bV >> 8) == 0 && (cV >> 8) == 0 && (dV >> 8) == 0;
- 
         return vVal;
-    }
-}
-// this recursion is too hard, not understand
-class Solution {
-    public ArrayList<String> restoreIpAddresses(String s) {
-        // Start typing your Java solution below
-        // DO NOT write main() function		
-		ArrayList<String> res = restoreIPAddresses(s, 4);
-        if(res == null) 
-			res = new ArrayList<String>();
-        return res;
-    }
-    public ArrayList<String> restoreIPAddresses(String s, int k){
-        assert(k <= 4 && k >= 1);
-        if(s == null || s.length() < k || s.length() > 3 * k) 
-			return null;
-		
-        ArrayList<String> res = new ArrayList<String>();        
-        
-        for(int i=0; i<Math.min(s.length(), 3); i++) {
-            String num = s.substring(0, i+1);
-            if((i == 0 || num.charAt(0) > '0') && Integer.parseInt(num) <= 255) {
-                if (k == 1) {
-                    if(i == s.length() - 1) 
-                        res.add(num);
-                }
-				else {
-                    ArrayList<String> remain = restoreIPAddresses(s.substring(i+1), k-1);
-                    if (remain != null) {
-                        for (String r : remain) {
-                            String temp = num + '.' + r;
-                            res.add(temp);
-                        }
-                    }
-                }
-            }
-			else
-                break;
-        }
-        return res;
-    }
-}
-
-// recursion
-class Solution {
-    public ArrayList<String> restoreIpAddresses(String s) {		
-		ArrayList<String> res = new ArrayList<String>();
-		String tmp = new String();
-		ip (s, tmp, res, 0, 0); 
-		if (res == null) 
-			return new ArrayList<String>();
-		return res;
-	}
-		
-	private void ip(String s, String tmp, ArrayList<String> res, int startPos, int partition) {
-        if (startPos == s.length() && partition == 4) {  
-            res.add(tmp);
-			return;
-        }
-		
-        int x = 0;  
-        for (int i = startPos; i < startPos+4 && i < s.length(); i++) {  
-            x = x * 10 + s.charAt(i) - '0';  
-            if (x > 255 || x < 0)
-                break;  
-              
-            String str = tmp;  
-            if (partition > 0)  
-                // str.append(1, '.');
-				str += '.';  
-              
-            String st = s.substring(startPos, i-startPos+1);
-			// judge if it is a valid ip address		
-            if (st.length() > 1 && st.charAt(0) == '0')  
-                break;  
-                  
-            str += st;  
-            ip (s, str, res, i+1, partition+1);
-		}  
     }
 }
