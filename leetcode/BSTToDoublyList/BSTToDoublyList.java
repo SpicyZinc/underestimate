48f5a5586ad758aa0124b42f68bea21b984b4623
Spicy Zinc <angxin880@gmail.com>
2018-12-21 06:34:42 -0800

    12/21/18 Sunnyvale to do bst to doublylist from China

--- leetcode/BSTToDoublyList/BSTToDoublyList.java
+++ leetcode/BSTToDoublyList/BSTToDoublyList.java
@@ -1,23 +1,38 @@
-/**
- * Definition of TreeNode:
- * public class TreeNode {
- *   public int val;
- *   public TreeNode left, right;
- *   public TreeNode(int val) {
- *     this.val = val;
- *     this.left = this.right = null;
- *   }
- * }
- * Definition for Doubly-ListNode.
- * public class DoublyListNode {
- *   int val;
- *   DoublyListNode next, prev;
- *   DoublyListNode(int val) {
- *     this.val = val;
- *     this.next = this.prev = null;
- *   }
- * }
- */
+/*
+Convert a binary search tree to doubly linked list with in-order traversal.
+
+Example
+Given a binary search tree:
+    4
+   / \
+  2   5
+ / \
+1   3
+return 1<->2<->3<->4<->5
+
+idea:
+
+*/
+
+// Definition of TreeNode
+class TreeNode {
+	public int val;
+	public TreeNode left, right;
+	public TreeNode(int val) {
+		this.val = val;
+		this.left = this.right = null;
+	}
+}
+// Definition for Doubly-ListNode
+class DoublyListNode {
+	int val;
+	DoublyListNode next, prev;
+	DoublyListNode(int val) {
+		this.val = val;
+		this.next = this.prev = null;
+	}
+}
+
 public class BSTToDoublyList {
     /**
      * @param root: The root of tree
@@ -28,7 +43,8 @@ public class BSTToDoublyList {
             return null;
         }
 
-        DoublyListNode left = null, right = null;
+        DoublyListNode left = null;
+        DoublyListNode right = null;
 
         if (root.left != null) {
             left = bstToDoublyList(root.left);
@@ -45,29 +61,35 @@ public class BSTToDoublyList {
             right = bstToDoublyList(root.right);
         }
 
-
         node.next = right;
         if (node.next != null) {
             node.next.prev = node;
         }
 
-
         return leftMost(node);
     }
 
     private DoublyListNode leftMost(DoublyListNode node) {
-        if (node == null) return null;
+        if (node == null) {
+        	return null;
+        }
+
         while (node.prev != null) {
             node = node.prev;
         }
+
         return node;
     }
 
     private DoublyListNode rightMost(DoublyListNode node) {
-        if (node == null) return null;
+        if (node == null) {
+        	return null;
+        }
+
         while (node.next != null) {
             node = node.next;
         }
+
         return node;
     }
 }
